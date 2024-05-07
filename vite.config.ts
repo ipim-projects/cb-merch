069e3c3ec243c629e6aff10a86f1feda9df6edb3
Ilya Pimenov <ipimenov@gmail.com>
2024-05-07 22:35:30 +0300

    vite no ssl

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), basicSsl()],
-  // plugins: [react()],
+  plugins: [react()],
   server: {
-    https: true,
     strictPort: true,
     // port: 5173,
   }
