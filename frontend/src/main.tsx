36059e8f49c0032f94c79bc300d533b9267505ec
Ilya Pimenov <ipimenov@gmail.com>
2024-05-08 14:21:49 +0300

    React.StrictMode

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 
@@ -8,7 +9,9 @@ const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Failed to find the root element');
 const root = ReactDOM.createRoot(rootElement);
 root.render(
-  <Provider store={store}>
-    <App/>
-  </Provider>
+  <React.StrictMode>
+    <Provider store={store}>
+      <App/>
+    </Provider>
+  </React.StrictMode>
 );
