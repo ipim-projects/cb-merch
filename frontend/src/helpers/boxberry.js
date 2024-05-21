57d0ccbd4a4f81b7ca5ed29ebf35233a13286c6f
Ilya Pimenov <ipimenov@gmail.com>
2024-05-21 13:16:58 +0300

    Boxberry api-token

--- frontend/src/helpers/boxberry.js
+++ frontend/src/helpers/boxberry.js
@@ -1,6 +1,13 @@
 const showBoxberryMap = callback => {
   boxberry.openOnPage('boxberry_map');
-  boxberry.open(callback);
+  boxberry.open(
+    callback,
+    '1$ecec1b9c26bd9fa11d1a72e4851184ba',
+    '',
+    '',
+    1000,
+    500
+  );
   /*boxberry.open(boxberryCallback, 'm2FltAKjbXQBLa2xqZ4sPQ==','Москва','', 1000, 500,
     0, 50, 50, 50);*/
 }
